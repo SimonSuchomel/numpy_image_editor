bbe5fee341c5a13995d5b231f1f730849209f40f
suchosim <suchosim@cvut.cz>
2025-01-07 17:54:03 +0100

    Refactor application from Tkinter GUI to CherryPy web server framework

--- main.py
+++ main.py
@@ -1,55 +1,49 @@
-from cProfile import label
-from tkinter import filedialog
-
 import numpy as np
-import tkinter as tk
+import cherrypy
+from PIL import Image
 
-class ImageEditor:
-    def __init__(self):
-        self.root = root
-        self.root.title("Image Editor")
-        self.root.minsize(600, 400)
+class WebServer:
+    @cherrypy.expose
+    def index(self):
+        return f'''
+        <html>
+        <head>
+            <title>Image Editor</title>
+        </head>
+        <body>
+            <h1>Image Editor</h1>
+            <form action="/open_file">
+                <input type="file" >
+            </form>
+            
+            <!-- Temporary Part (WIP) -->
 
-        frame1 = tk.Frame(self.root)
-        frame2 = tk.Frame(self.root)
-        self.root.grid_rowconfigure(0, weight=1)  # Single row
-        self.root.grid_columnconfigure(0, weight=1, uniform="frame")  # Frame 1 takes 1/4 of the horizontal space
-        self.root.grid_columnconfigure(1, weight=3, uniform="frame")  # Frame 2 takes 3/4 of the horizontal space
+            <div>
+                <h2>Adjust Image</h2>
+                <form action="/adjust_size">
+                    <input type="submit" value="Resize">
+                </form>
+                <form action="/brightness">
+                    <input type="submit" value="Adjust Brightness">
+                </form>
+                <form action="/solarize">
+                    <input type="submit" value="Solarize">
+                </form>
+                <form action="/negative">
+                    <input type="submit" value="Convert to Negative">
+                </form>
+                <form action="/highlight_edges">
+                    <input type="submit" value="Highlight Edges"> 
+                </form>
+            </div>
+
+            <h2>Image Preview:</h2>
+
+        </body>
+        </html>
+        '''
+
+if __name__ == '__main__':
+    cherrypy.quickstart(WebServer())
 
 
-        # Frame 1
-        button1 = tk.Button(frame1, text="Adjust Size")
-        button2 = tk.Button(frame1, text="Brightness")
-        button3 = tk.Button(frame1, text="Solarize")
-        button4 = tk.Button(frame1, text="Convert to Negative")
-        button5 = tk.Button(frame1, text="Highlight Edges")
-
-        button1.pack(fill='x', side='bottom')
-        button2.pack(fill='x', side='bottom')
-        button3.pack(fill='x', side='bottom')
-        button4.pack(fill='x', side='bottom')
-        button5.pack(fill='x', side='bottom')
-
-        # entry1 = tk.Entry(frame1)
-        # label1 = tk.Label(frame1, text="Label1")
-
-
-        # Frame 2 (larger)
-        button6 = tk.Button(frame2, text="Open File", command=self.open_file)
-
-        button6.pack(fill='x', side='top')
-
-
-        # Grid init
-        frame1.grid(row=0, column=0, sticky="nsew")
-        frame2.grid(row=0, column=1, sticky="nsew")
-
-
-    def open_file(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Image File", "*.png;*.jpg;*.pdf")])
-        print(file_path)
-
-# Main Window
-root = tk.Tk() # instance of the main window
-editor = ImageEditor()
-root.mainloop()
